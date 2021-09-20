48eb7ba238ef6d65ee75597dfac855e25eb4e584
Dawar <dawaraftabdev@gmail.com>
2021-09-20 23:31:33 +0500

    Change

--- src/components/buttons/styles.js
+++ src/components/buttons/styles.js
@@ -1,13 +1,3 @@
 import styled from 'styled-components'
 import { Button } from '@theguarantors/ui-kit-v3'
-export const StyledButton = styled(Button)`
-  font-family: Gilroy;
-  font-size: 16px;
-  line-height: 24px;
-  text-align: center;
-  color: #a6a6bf;
-  padding: 6px 16px;
-  margin-top: 24px;
-  background: #ededf2;
-  border-radius: 4px;
-`
+export const StyledButton = styled(Button)``
